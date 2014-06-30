7ff2a0da56fc163f71f3969e96328516ff07aaca
David <david@DaviddeMacBook-Air.local>
2014-06-30 15:15:53 +0800

    fix the comment

--- TaskWorker/config.py
+++ TaskWorker/config.py
@@ -3,8 +3,8 @@ import os
 LOGIN_TOKEN_PREFIX  = "TOKEN:"
 DEFAULT_LOGIN_EXPIRE_SECONDS = 60*60*24
 
-# store your AUTH_KEY in os via add app.yaml
+# store your TASK_WORKER_AUTH_KEY in os via add app.yaml
 # env_variables:
-#   AUTH_KEY: 'YOUR AUTH KEY'
+#   TASK_WORKER_AUTH_KEY: 'YOUR AUTH KEY'
 
 AUTH_KEY = os.environ.get("TASK_WORKER_AUTH_KEY")
